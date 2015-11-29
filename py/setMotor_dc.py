07cd1b7c6730df59fc8e8ab129f2af7e97447382
bluenex <tulakan.r@gmail.com>
2015-11-29 18:48:15 +0700

    little tweak for setdc

--- py/setMotor_dc.py
+++ py/setMotor_dc.py
@@ -4,12 +4,14 @@ sys.path.insert(0, 'D:/tulfolder/LocalProjects/dllForUnity/dcPythonLib')
 import dcAction as dc
 import time
 
+import threading
+
 # connection
 port = 'COM21'
 baudrate = 115200
 # define variables
 stepping = 1 # sec
-initPWM = 70
+initPWM = 80
 stepPWM = 10
 targetPWM = 140
 incomingPWM = 0
@@ -35,13 +37,16 @@ while True:
     start = time.time()
     while (initPWM != incomingPWM):
         if (time.time() - start > stepping):
+            if (initPWM <= incomingPWM):
+                dcMotor.WritePWM(initPWM)
+                print "speed now: " + str(initPWM)
+            else:
+                dcMotor.WritePWM(incomingPWM)
+                print "speed now: " + str(incomingPWM)
+                break
             initPWM += stepPWM
-            dcMotor.WritePWM(initPWM)
-            time.sleep(stepping)
-            print "speed now: " + str(initPWM)
             start = time.time()
-
-    initPWM = 70
+    initPWM = 80
     incomingPWM = 0
 
 dcMotor.Close()
